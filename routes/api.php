83827e8a81460125c6ea817fd43b50e3be1ab642
richzago97 <rich.czago@gmail.com>
2023-11-07 16:27:13 -0300

    feat: agrupado a rota de auth para futuras novas rotas

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,15 @@
 <?php
 
-use App\Http\Controllers\UserController;
+use App\Http\Controllers\{AuthController, UserController};
 use Illuminate\Support\Facades\Route;
 
 Route::post('/users', [UserController::class, 'create']);
+
+
+Route::group([
+    'prefix' => 'auth'
+], function () {
+    Route::post('/login', [AuthController::class, 'login']);
+    // Route::post('/forget-password', [AuthController::class, 'forgetPassword']);
+    // Route::post('/send-password', [AuthController::class, 'sendPassword']);
+});
