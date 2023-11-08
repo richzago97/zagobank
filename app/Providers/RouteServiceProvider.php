746ead9000eecc941f300f8086b9fe143327eaac
richzago97 <rich.czago@gmail.com>
2023-11-08 13:37:37 -0300

    feat: modificado o caminho das routes

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Cache\RateLimiting\Limit;
 use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\Facades\Route;
 
@@ -29,9 +30,21 @@ class RouteServiceProvider extends ServiceProvider
         });
 
         $this->routes(function () {
-            Route::middleware('api')
-                ->prefix('api')
-                ->group(base_path('routes/api.php'));
+            // Log::debug(base_path('routes/api'));
+            $dirs = scandir($path = base_path('routes/api'));
+            $dirsWithoutDot = array_diff($dirs, ['..', '.']);
+            // Log::debug('Dirs', $dirs);
+            // Log::debug('Dirs', $dirsWithoutDot);
+
+            foreach ($dirsWithoutDot as $dir) {
+                if (file_exists($file = "{$path}/{$dir}")) {
+                    Route::middleware('api')
+                        ->prefix('api')
+                        ->group($file);
+                }
+            }
+
+
 
             Route::middleware('web')
                 ->group(base_path('routes/web.php'));
