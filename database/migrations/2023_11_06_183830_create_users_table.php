d413d4a011eb5e7f3c36420e21bf672577021c98
richzago97 <rich.czago@gmail.com>
2023-11-08 13:37:57 -0300

    feat: criado a migração da tabela users

--- database/migrations/2023_11_06_183830_create_users_table.php
+++ database/migrations/2023_11_06_183830_create_users_table.php
@@ -20,6 +20,8 @@ return new class extends Migration
             $table->string('type');
             $table->decimal('balance')->default(0);
             $table->timestamps();
+
+            $table->primary('id');
         });
     }
 
